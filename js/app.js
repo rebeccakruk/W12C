0a5e5c2266de5c65cca649dc87538c024576b053
rk <rebeccakruk@outlook.com>
2022-11-26 18:30:27 -0700

    done

--- js/app.js
+++ js/app.js
@@ -1,62 +1,70 @@
-function sendPost(){
-axios.request({
-    url: "https://jsonplaceholder.typicode.com/posts/1/comments",
-    method: `POST`,
-    data: {
-        title: document.getElementById(`postTitle`).value,
-        body: document.getElementById(`postBody`).value,
-        userId: document.getElementById(`userId`).value,
-    }
-}).then(postSuccess).catch(postFailure);
+function sendPost() {
+    axios
+        .request({
+            url: "https://jsonplaceholder.typicode.com/posts/1/comments",
+            method: `POST`,
+            data: {
+                title: document.getElementById(`postTitle`).value,
+                body: document.getElementById(`postBody`).value,
+                userId: document.getElementById(`userId`).value,
+            },
+        })
+        .then(postSuccess)
+        .catch(postFailure);
 }
 
-function postSuccess(response){
+function postSuccess(response) {
     let data = response.data;
-    Cookies.set(`sessionData`, data)
+    Cookies.set(`sessionData`, data);
     document.getElementById(`postResults`).innerHTML = `<h4>Hello ${data.userId}, your review titled "${data.title}", has been successfully submitted! Thanks for your feedback.</h4>`;
-    document.getElementById(`reviewBox`).innerHTML = `<h3>${data.body}</h3>`
-    // console.log(data);
+    document.getElementById(`reviewBox`).innerHTML = `<h3>${data.body}</h3>`;
 }
 
-function postFailure(error){
+function postFailure(error) {
     console.log(error);
     document.getElementById(`postResults`).innerHTML = `<h3>Something went wrong. Your feedback is important to us. Please try again later.</h3>`;
 }
 
-function updatePost(){
-axios.request({
-    url: "https://jsonplaceholder.typicode.com/posts/1/comments",
-    method: `PATCH`,
-    data: {
-        title: document.getElementById(`postTitle`).value,
-        body: document.getElementById(`postBody`).value,
-    }
-}).then(patchSuccess).catch(patchFailure);
+function updatePost() {
+    axios
+        .request({
+            url: "https://jsonplaceholder.typicode.com/posts/1/",
+            method: `PATCH`,
+            data: {
+                title: document.getElementById(`postTitle`).value,
+                body: document.getElementById(`postBody`).value,
+            },
+        })
+        .then(patchSuccess)
+        .catch(patchFailure);
 }
 
-function patchSuccess(response){
+function patchSuccess(response) {
     clearResults();
     let update = response.data;
     console.log(update);
-    document.getElementById(`postResults`).insertAdjacentHTML(`beforeend`, `<h3> our review has been updated: ${update.title}.</h3>`);
+    document.getElementById(`postResults`).insertAdjacentHTML(`beforeend`, `<h3> Your review has been updated: ${update.title}.</h3>`);
     document.getElementById(`reviewBox`).insertAdjacentHTML(`afterbegin`, `<h4>${update.body}</h4>`);
 }
 
-function patchFailure(error){
+function patchFailure(error) {
     console.log(error);
     document.getElementById(`postResults`).innerHTML = `<h3>Something went wrong. Your feedback is important to us. Please try again later.</h3>`;
 }
 
-function deletePost(){
-axios.request({
-    url: "https://jsonplaceholder.typicode.com/posts/1",
-    method: `DELETE`,
-    data: {
-         title: document.getElementById(`postTitle`).value,
-        body: document.getElementById(`postBody`).value,
-        userId: document.getElementById(`userId`).value,
-    }
-}).then(deleteSuccess).catch(deleteFailure)
+function deletePost() {
+    axios
+        .request({
+            url: "https://jsonplaceholder.typicode.com/posts/1",
+            method: `DELETE`,
+            data: {
+                title: document.getElementById(`postTitle`).value,
+                body: document.getElementById(`postBody`).value,
+                userId: document.getElementById(`userId`).value,
+            },
+        })
+        .then(deleteSuccess)
+        .catch(deleteFailure);
 }
 
 function deleteSuccess(response) {
@@ -64,86 +72,84 @@ function deleteSuccess(response) {
     clearResults();
     document.getElementById(`userId`).value = "";
     document.getElementById(`postResults`).innerHTML = `<h3>Your info has successfully been deleted.</h3>`;
+    document.getElementById(`post`).innerHTML = "";
+    document.getElementById(`commentary`).innerHTML = "";
 }
 
 function deleteFailure(error) {
     console.log(error);
+    document.getElementById(`postResults`).innerHTML = `<h3>Something went wrong. Your feedback is important to us. Please try again later.</h3>`;
 }
 
-window.onload = function getPost(){
-axios.request({
-    url: "https://jsonplaceholder.typicode.com/posts",
-    method: `GET`,
-// },
-// function addComments(){
-// axios.request({
-//     url: "https://jsonplaceholder.typicode.com/posts/1/comments",
-//     method: `PUT`,
-//     data: {
-//         comments: [{
-//             name: '...',
-//             email: '...',
-//             body: '...',
-//         },
-//     ]}
-// }).then(commentSuccess).catch(commentFailure);
-// }
-}).then(getSuccess).catch(getFailure);
+window.onload = function getPost() {
+    axios
+        .request({
+            url: "https://jsonplaceholder.typicode.com/posts",
+            method: `GET`,
+        })
+        .then(getSuccess)
+        .catch(getFailure);
+};
+
+function addComments() {
+    axios
+        .request({
+            url: "https://jsonplaceholder.typicode.com/posts/1/comments",
+            method: `GET`,
+            data: {
+                comments: [
+                    {
+                        // postID: '...',
+                        // id: '...',
+                        // name: '...',
+                        // email: '...',
+                        // body: '...',
+                    },
+                ],
+            },
+        })
+        .then(commentSuccess)
+        .catch(commentFailure);
 }
 
-function getSuccess(response){
+function getSuccess(response) {
+    // addComments();
     let posts = response.data;
-    for (let post of posts)
-    document.getElementById(`getPosts`).insertAdjacentHTML(`beforeend`, `<p>Review Title:${post.title} <br> Review:${post.body} <br> User:${post.userId} <br> Comments:${post.comments}</p>`);
-}
-
-function addComments(){
-axios.request({
-    url: "https://jsonplaceholder.typicode.com/posts/1/comments",
-    method: `GET`,
-    data: {
-        comments: [{
-            // postID: '...',
-            // id: '...',
-            // name: '...',
-            // email: '...',
-            // body: '...',
-        },
-    ]}
-}).then(commentSuccess).catch(commentFailure);
+    for (let post of posts) {
+        document.getElementById(`post`).insertAdjacentHTML(`afterbegin`, `<p>Review Title:${post.title} <br> Review: ${post.body} <br> User: ${post.userId}</p>`);
+    }
+    addComments();
 }
 
 function commentSuccess(response) {
-    let extra = response.data;
-    console.log(response);
-    for (let comment of extra){
-        document.getElementById(`getPosts`).insertAdjacentHTML(`beforeend`, `<h5>Comments: <br>Post ID: ${comment[`postId`]} <br> Name: ${comment[`name`]} <br>${comment[`body`]}</h5>`);
+    let data = response.data;
+    for (let comment of data) {
+        document.getElementById(`commentary`).insertAdjacentHTML(`beforeend`, `<p>Comments: <br>Post ID: ${comment[`postId`]} <br> Name: ${comment[`name`]} <br>${comment[`body`]}</p>`);
     }
 }
 
-function commentFailure(error){
+function getFailure(error) {
     console.log(error);
+    document.getElementById(`postResults`).innerHTML = `<h3>Something went wrong. Your feedback is important to us. Please try again later.</h3>`;
 }
 
-function getFailure(error){
+function commentFailure(error) {
     console.log(error);
+    document.getElementById(`postResults`).innerHTML = `<h3>Something went wrong. Your feedback is important to us. Please try again later.</h3>`;
 }
 
-function clearPost(){
+function clearPost() {
     document.getElementById(`postTitle`).value = "";
     document.getElementById(`postBody`).value = "";
 }
 
-function clearResults(){
+function clearResults() {
     document.getElementById(`postResults`).innerHTML = "";
     document.getElementById(`reviewBox`).innerHTML = "";
 }
-
 
 document.getElementById(`submit`).addEventListener(`click`, sendPost);
 document.getElementById(`updatePost`).addEventListener(`click`, updatePost);
 document.getElementById(`clearPost`).addEventListener(`click`, clearPost);
 document.getElementById(`deleteAll`).addEventListener(`click`, deletePost);
-document.getElementById(`comments`).addEventListener(`click`, addComments);
-// window.onload = addComments();
 
